95156f8007a181c1cd35d1752833a0791c72c6a7
BenoitSafari <gregory.michalak.192@gmail.com>
2025-04-29 16:48:36 +0200

    feat(nuxt): useHost: protocol definition

--- nuxt/composables/use-host.ts
+++ nuxt/composables/use-host.ts
@@ -1,4 +1,11 @@
 export const useHost = () => {
-    const host = window?.location?.host ?? useRequestHeaders()?.['host'] ?? '';
-    return { host };
+    const isClient = typeof window !== 'undefined';
+
+    const host = isClient ? window.location.host : (useRequestHeaders()?.['host'] ?? '');
+
+    const protocol = isClient
+        ? window.location.protocol.replace(':', '')
+        : (useRequestHeaders()?.['x-forwarded-proto'] ?? 'http');
+
+    return { host, protocol };
 };
