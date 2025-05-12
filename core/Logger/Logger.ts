2fef032748b4e441e3622b550ba6b1ff1dd1389c
Dylan <dromarddylan@gmail.com>
2025-05-12 10:15:11 +0200

    fix(logger): prevent window is not defined during SSR

--- core/Logger/Logger.ts
+++ core/Logger/Logger.ts
@@ -1,6 +1,6 @@
 export class Logger {
     public static devLog(...params: any): void {
-        if (process?.env?.NODE_ENV === 'development' || (window && import.meta.env?.DEV)) {
+        if ((process?.env?.NODE_ENV === 'development' || import.meta.env?.DEV) && typeof window !== 'undefined') {
             console.log(`${new Date().toLocaleTimeString()} |`, ...params);
         }
     }
