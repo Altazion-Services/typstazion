6793050825570f553c649ef8a7407a5f98d89c94
BenoitSafari <gregory.michalak.192@gmail.com>
2025-05-28 12:46:27 +0200

    refactor(Logger): Changed the way we ignore errors

--- core/Logger/Logger.ts
+++ core/Logger/Logger.ts
@@ -4,4 +4,20 @@ export class Logger {
             console.log(`${new Date().toLocaleTimeString()} |`, ...params);
         }
     }
+
+    public static setupLogFilter(filter: (msg: string) => boolean) {
+        function override(fnName: 'log' | 'warn' | 'error') {
+            const original = console[fnName].bind(console);
+            console[fnName] = (...args: any[]) => {
+                if (filter(args.map(a => (typeof a === 'string' ? a : JSON.stringify(a))).join(' '))) {
+                    return;
+                }
+                original(...args);
+            };
+        }
+
+        override('log');
+        override('warn');
+        override('error');
+    }
 }
--- nuxt/plugins/types.ts
+++
@@ -1,5 +0,0 @@
-import type { ObjectPlugin, Plugin, App } from 'vue';
-
-export type NuxtAppInterface = {
-    vueApp: App<Element>;
-};
